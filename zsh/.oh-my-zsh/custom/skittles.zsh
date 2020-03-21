2843e0aa7988d421c36fcd874c88e6e1ff0e1983
Rhyse Simpson <skittles9832@gmail.com>
2020-03-21 22:08:40 +1100

    zsh: update toaac function

--- zsh/.oh-my-zsh/custom/skittles.zsh
+++ zsh/.oh-my-zsh/custom/skittles.zsh
@@ -3,4 +3,8 @@ HOSTNAME=FedoraBox
 
 alias neofetch='neofetch --config ~/neofetch.conf'
 
-toaac(){ find -name "*.$1" -exec ffmpeg -i {} -c:a aac -c:v copy -b:a "$2"k {}.m4a \; }
+toaac(){
+  find -name "*.$1" -exec ffmpeg -i {} -c:a aac -c:v copy -b:a "$2"k {}.m4a \;
+  find -name "*.$1.m4a" -exec rename ".$1" "" {} \;
+  find -name "*.$1" -exec rm -rf {} \;
+}
